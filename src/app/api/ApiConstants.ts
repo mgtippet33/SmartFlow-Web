cad2c10aa8beacc4089cebc7702586bc49fe1950
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-12-01 23:02:57 +0200

    added login and register pages added user model added apiconstants and http service

---
+++ src/app/api/ApiConstants.ts
@@ -0,0 +1,5 @@
+export enum ApiConstants {
+    main_url = "https://localhost:44324/",
+    register_url = "signUp/",
+    login_url = "signIn/"
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
